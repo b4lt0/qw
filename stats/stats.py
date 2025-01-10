81a217e7cef0f57511cfde5f046e55c3b3e7745a
andreabalillo <andrea.balillo@gmail.com>
2025-01-10 11:48:10 +0100

    use backend tkagg for matplotlib

--- stats/stats.py
+++ stats/stats.py
@@ -1,6 +1,8 @@
 import os
 import json
 import argparse
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description='Process a qlog file.')
