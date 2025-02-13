831d97cea0295a3761ec518359767a3c4ebe6016
andreabalillo <andrea.balillo@gmail.com>
2025-02-13 11:31:47 +0100

    set sigmoid up to 40 steps

--- proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/Westwood.cpp
+++ proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/Westwood.cpp
@@ -350,7 +350,7 @@ namespace quic {
 
     uint32_t Westwood::westwoodLowPassFilter(uint32_t a, uint32_t b) {
         // Parameters to shape the sigmoid:
-        constexpr float center = 10.0f;  // Midpoint: sigmoid(center) = 0.5
+        constexpr float center = 20.0f;  // Midpoint: sigmoid(center) = 0.5
         constexpr float scale  = 2.0f;   // Controls the steepness of the sigmoid
 
         float s = static_cast<float>(step_);
