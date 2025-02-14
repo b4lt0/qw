44f46f203f3d76ff3fed76898e181f20eedd4ebb
andreabalillo <andrea.balillo@gmail.com>
2025-02-14 11:13:58 +0100

    MODIFIED SIGMOID 16 1

--- proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/Westwood.cpp
+++ proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/Westwood.cpp
@@ -350,8 +350,8 @@ namespace quic {
 
     uint32_t Westwood::westwoodLowPassFilter(uint32_t a, uint32_t b) { //20 midpoint 0.5 steepness
         // Parameters to shape the sigmoid:
-        constexpr float center = 5.0f;  // Midpoint: sigmoid(center) = 0.5
-        constexpr float scale  = 0.5f;   // Controls the steepness of the sigmoid
+        constexpr float center = 16.0f;  // Midpoint: sigmoid(center) = 0.5
+        constexpr float scale  = 1.0f;   // Controls the steepness of the sigmoid
 
         float s = static_cast<float>(step_);
         // Compute the sigmoid function:
