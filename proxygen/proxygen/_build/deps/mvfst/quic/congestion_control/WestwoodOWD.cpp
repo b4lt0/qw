3855b801e308db2e6767cfdf9ff9520f58772897
andreabalillo <andrea.balillo@gmail.com>
2025-03-10 18:14:43 +0100

    fix

--- proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/WestwoodOWD.cpp
+++ proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/WestwoodOWD.cpp
@@ -92,8 +92,8 @@ WestwoodOWD::WestwoodOWD(QuicConnectionStateBase &conn)
       interArrival_(0),
       owdv_(0),
       owd_(0),
-      lossMaxRtt_(0) 
-      
+      lossMaxRtt_(0) {
+
     cwndBytes_ = boundedCwnd(
         cwndBytes_,
         quicConnectionState_.udpSendPacketLen,
