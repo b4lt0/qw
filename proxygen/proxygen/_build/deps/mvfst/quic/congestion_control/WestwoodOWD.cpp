a1e2b7aba25d0d319ee6a468eb749feab8d97b77
andreabalillo <andrea.balillo@gmail.com>
2025-03-12 14:28:28 +0100

    setting owd cwnd

--- proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/WestwoodOWD.cpp
+++ proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/WestwoodOWD.cpp
@@ -192,20 +192,12 @@ void WestwoodOWD::updateOneWayDelay(const CongestionController::AckEvent::AckPac
         latestSendTimeStamp_ = packet.outstandingPacketMetadata.time;
         return;
     }
-    
-    // Save previous timestamps for logging.
-    auto prevSendTimeStamp = latestSendTimeStamp_;
-    auto prevReceiveTimeStamp = latestReceiveTimeStamp_;
-    
     auto currentSendTimeStamp = packet.outstandingPacketMetadata.time;
-    interDeparture_ = std::chrono::duration_cast<std::chrono::microseconds>(
-                           currentSendTimeStamp - prevSendTimeStamp
-                       ).count();
+    interDeparture_ = std::chrono::duration_cast<std::chrono::microseconds>(currentSendTimeStamp - latestSendTimeStamp_).count();
 
     auto currentReceiveTimeStamp = packet.receiveRelativeTimeStampUsec.value().count();
-    interArrival_ = currentReceiveTimeStamp - prevReceiveTimeStamp;
+    interArrival_ = currentReceiveTimeStamp - latestReceiveTimeStamp_;
 
-    // Update the latest timestamps.
     latestSendTimeStamp_ = currentSendTimeStamp;
     latestReceiveTimeStamp_ = currentReceiveTimeStamp;
 
@@ -216,23 +208,34 @@ void WestwoodOWD::updateOneWayDelay(const CongestionController::AckEvent::AckPac
     owdv_ = interArrival_ - interDeparture_;
     owd_ += owdv_;
 
-    VLOG(1) << "-------------------------";
-    VLOG(1) << "Packet Number   : " << packet.packetNum;
-    VLOG(1) << "Inter-arrival   : " << interArrival_
-            << " = " << currentReceiveTimeStamp << " - " << prevReceiveTimeStamp;
-    VLOG(1) << "Inter-departure : " << interDeparture_
-            << " = " 
-            << std::chrono::duration_cast<std::chrono::microseconds>(currentSendTimeStamp.time_since_epoch()).count()
-            << " - " 
-            << std::chrono::duration_cast<std::chrono::microseconds>(prevSendTimeStamp.time_since_epoch()).count();
-    VLOG(1) << "OWD variation   : " << owdv_
-            << " = " << interArrival_ << " - " << interDeparture_;
-    VLOG(1) << "One-way-delay   : " << owd_ << " (updated with " << owdv_ << ")";
-    VLOG(1) << "-------------------------";
+    /** 
+    * trying to clamp owd in order to reject nonsense queue negative levels
+    * caused by out‐of‐order arrivals that produce apparent negative gaps 
+    * and not reflecting actual queue empting. 
+    **/
+    owd_ = std::max(static_cast<int64_t>(0), owd_);
+
+    // std::cout << packet.packetNum << " " << currentSendTimeStamp << " " << currentReceiveTimeStamp << std::endl; 
+
+    // VLOG(1) << "-------------------------";
+    // VLOG(1) << "Packet Number   :" << packet.packetNum;
+    // VLOG(1) << "Inter-departure : " << interArrival_ << " = " 
+    //     << std::chrono::duration_cast<std::chrono::microseconds>(currentReceiveTimeStamp.time_since_epoch()).count()
+    //     << " - "
+    //     << std::chrono::duration_cast<std::chrono::microseconds>(latestReceiveTimeStamp_.time_since_epoch()).count();
+
+    // VLOG(1) << "Inter-departure : " << interDeparture_ << " = " 
+    //     << std::chrono::duration_cast<std::chrono::microseconds>(currentSendTimeStamp.time_since_epoch()).count()
+    //     << " - "
+    //     << std::chrono::duration_cast<std::chrono::microseconds>(latestSendTimeStamp_.time_since_epoch()).count();
+
+    // VLOG(1) << "OWD variation   : " << owdv_ << " = " << interArrival_ << " - " << interDeparture_;
+    // VLOG(1) << "One-way-delay   : " << owd_ << " += " << owdv_;
+    // VLOG(1) << "-------------------------";
+
 
     std::cout << time_owd_us << " " << owd_ << " " << owdv_ << " " << lossMaxRtt_.count() << std::endl;
 }
-
 
 
 
@@ -256,18 +259,18 @@ void WestwoodOWD::onPacketAcked(const CongestionController::AckEvent::AckPacket 
     }
 
     // If the delay condition is met, adjust ssthresh and cwnd.
-    // if (delayControl(0.5)) {
-    //     uint64_t rttMinUs = rttSampler_.minRtt().count();
-    //     ssthresh_ = std::max(
-    //         static_cast<uint64_t>((bandwidthEstimate_ * rttMinUs / 1.0e6)),
-    //         2 * quicConnectionState_.udpSendPacketLen);
-    //     cwndBytes_ = ssthresh_;
-    //     cwndBytes_ = boundedCwnd(
-    //         cwndBytes_,
-    //         quicConnectionState_.udpSendPacketLen,
-    //         quicConnectionState_.transportSettings.maxCwndInMss,
-    //         quicConnectionState_.transportSettings.minCwndInMss);
-    // }
+    if (delayControl(0.5)) {
+        uint64_t rttMinUs = rttSampler_.minRtt().count();
+        ssthresh_ = std::max(
+            static_cast<uint64_t>((bandwidthEstimate_ * rttMinUs / 1.0e6)),
+            2 * quicConnectionState_.udpSendPacketLen);
+        cwndBytes_ = ssthresh_;
+        cwndBytes_ = boundedCwnd(
+            cwndBytes_,
+            quicConnectionState_.udpSendPacketLen,
+            quicConnectionState_.transportSettings.maxCwndInMss,
+            quicConnectionState_.transportSettings.minCwndInMss);
+    }
 
     // Slow start or congestion avoidance increment:
     if (cwndBytes_ < ssthresh_) {
@@ -299,8 +302,8 @@ void WestwoodOWD::onPacketLoss(const LossEvent &loss) {
     DCHECK(loss.largestLostPacketNum.has_value() && loss.largestLostSentTime.has_value());
     subtractAndCheckUnderflow(quicConnectionState_.lossState.inflightBytes, loss.lostBytes);
 
-    // owd_ = 0;
-    // owdv_ = 0;
+    owd_ = 0;
+    owdv_ = 0;
 
     // lossMaxRtt_ = rttSampler_.maxRtt();
 
