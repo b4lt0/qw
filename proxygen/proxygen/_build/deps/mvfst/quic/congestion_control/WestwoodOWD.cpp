37a0d90a4f09377cd6a11e7ae9f564db93959645
andreabalillo <andrea.balillo@gmail.com>
2025-04-07 11:18:39 +0200

    dc 50 80

--- proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/WestwoodOWD.cpp
+++ proxygen/proxygen/_build/deps/mvfst/quic/congestion_control/WestwoodOWD.cpp
@@ -247,7 +247,7 @@ void WestwoodOWD::onPacketAcked(const CongestionController::AckEvent::AckPacket 
             static_cast<uint64_t>((bandwidthEstimate_ * rttMinUs / 1.0e6)),
             2 * quicConnectionState_.udpSendPacketLen);*/
         ssthresh_ = std::max(
-            static_cast<uint64_t>(bandwidthEstimate_ * (rttMinUs + (0.3 * (lossMaxRtt_.count() - rttMinUs))) / 1.0e6),
+            static_cast<uint64_t>(bandwidthEstimate_ * (rttMinUs + (0.5 * (lossMaxRtt_.count() - rttMinUs))) / 1.0e6),
             2 * quicConnectionState_.udpSendPacketLen);
         cwndBytes_ = ssthresh_;
         cwndBytes_ = boundedCwnd(
@@ -258,7 +258,7 @@ void WestwoodOWD::onPacketAcked(const CongestionController::AckEvent::AckPacket 
 
         // owd_ = 0; these are for delay control 0
     
-        owd_ = 0.3 * (lossMaxRtt_.count() - rttMinUs);
+        owd_ = 0.5 * (lossMaxRtt_.count() - rttMinUs);
         owdv_ = 0;
 
         //lossMaxRtt_ = rttSampler_.maxRtt();
